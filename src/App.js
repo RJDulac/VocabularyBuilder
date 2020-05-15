ebd68da3693a804563767fec2869bcfe6f1334b0
machine <analyticrye@gmail.com>
2020-05-15 05:51:05 -0400

    project started. Connected to api

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    word: "react"
+  };
+  async componentDidMount() {
+    const response = await axios.get(
+      `https://dictionaryapi.com/api/v3/references/collegiate/json/${this.state.word}?key=${process.env.REACT_APP_WEBSTER_DICTIONARY_CLIENT_ID}`
+    );
+    console.log(response.data[0].shortdef);
+  }
+  render() {
+    return (
+      <div>
+        <h1>working</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
