af672d2f2b1fe5fe7125ba07bb2f4ddc74e04c40
machine <analyticrye@gmail.com>
2020-05-15 08:17:40 -0400

    improved bug fix but not ideal

--- src/App.js
+++ src/App.js
@@ -4,24 +4,31 @@ import SearchDictionary from "./components/SearchDictionary";
 
 class App extends Component {
   state = {
-    word: "dog",
+    word: "",
     definitions: []
   };
   async componentDidMount() {
     const response = await axios.get(
-      `https://dictionaryapi.com/api/v3/references/collegiate/json/${this.state.word}?key=${process.env.REACT_APP_WEBSTER_DICTIONARY_CLIENT_ID}`
-    );
-    console.log(response.data);
-  }
-  searchWord = async text => {
-    const response = await axios.get(
-      `https://dictionaryapi.com/api/v3/references/collegiate/json/${text}?key=${process.env.REACT_APP_WEBSTER_DICTIONARY_CLIENT_ID}`
+      `https://dictionaryapi.com/api/v3/references/collegiate/json/example?key=${process.env.REACT_APP_WEBSTER_DICTIONARY_CLIENT_ID}`
     );
 
     const definitions = response.data.map(def => def.shortdef);
 
-    console.log(response.data);
-    this.setState({ definitions });
+    this.setState({ definitions, word: "example" });
+  }
+  searchWord = async text => {
+    try {
+      const response = await axios.get(
+        `https://dictionaryapi.com/api/v3/references/collegiate/json/${text}?key=${process.env.REACT_APP_WEBSTER_DICTIONARY_CLIENT_ID}`
+      );
+
+      const definitions = response.data.map(def => def.shortdef);
+
+      console.log(definitions);
+      this.setState({ definitions, word: text });
+    } catch (error) {
+      console.log("Something went wrong", error);
+    }
   };
   render() {
     return (
@@ -33,15 +40,22 @@ class App extends Component {
         }}
       >
         <SearchDictionary searchWord={this.searchWord} />
-        {this.state.definitions.map(def => {
-          return def === undefined ? (
-            <p>word does not exit</p>
-          ) : (
-            def.map(def => {
-              return <p key={Math.random()}>{def}</p>;
-            })
-          );
-        })}
+        <p>
+          <strong>The defintions of {this.state.word} are:</strong>
+        </p>
+        {this.state.definitions.length === 0 ? (
+          <p>{this.state.word} not found</p>
+        ) : (
+          this.state.definitions.map(def => {
+            return def === undefined ? (
+              <p key={Math.random()}>{this.state.word} not found</p>
+            ) : (
+              def.map(def => {
+                return <p key={Math.random()}>{def}</p>;
+              })
+            );
+          })
+        )}
       </div>
     );
   }
