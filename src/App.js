bc25439836fe9f36dfa7b7b596fe9f32a0d06ae7
machine <analyticrye@gmail.com>
2020-05-15 06:26:06 -0400

    improved search to get all definitions and display them

--- src/App.js
+++ src/App.js
@@ -4,25 +4,30 @@ import SearchDictionary from "./components/SearchDictionary";
 
 class App extends Component {
   state = {
-    word: "react"
+    word: "dog",
+    definitions: []
   };
   async componentDidMount() {
     const response = await axios.get(
       `https://dictionaryapi.com/api/v3/references/collegiate/json/${this.state.word}?key=${process.env.REACT_APP_WEBSTER_DICTIONARY_CLIENT_ID}`
     );
-    console.log(response.data[0].shortdef);
+    console.log(response.data);
   }
   searchWord = async text => {
     const response = await axios.get(
       `https://dictionaryapi.com/api/v3/references/collegiate/json/${text}?key=${process.env.REACT_APP_WEBSTER_DICTIONARY_CLIENT_ID}`
     );
-    this.setState({ word: response.data[0].shortdef });
+    const definitions = response.data.map(def => def.shortdef);
+    console.log(definitions);
+    this.setState({ definitions });
   };
   render() {
     return (
       <div>
         <SearchDictionary searchWord={this.searchWord} />
-        <p>{this.state.word}</p>
+        {this.state.definitions.map(def => {
+          return <p key={Math.random()}>{def}</p>;
+        })}
       </div>
     );
   }
